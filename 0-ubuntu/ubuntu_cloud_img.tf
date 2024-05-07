28d0b9d25dfa026e32d8b769f0be7bd7c1d5b1df
Vincent Dupain <vdupain@gmail.com>
2024-05-07 23:05:11 +0200

    somes fix for ubuntu vms

---
+++ 0-ubuntu/ubuntu_cloud_img.tf
@@ -0,0 +1,9 @@
+resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = var.pve_target_node
+  overwrite = false
+  url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+  checksum = "da76b0ef1cd45939f0d14ca11dd4dfabbe13b94d2ec144d54c805d1baf852e67"
+  checksum_algorithm = "sha256"
+}
