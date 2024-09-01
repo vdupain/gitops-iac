b74fb194b13337bbef505c32711f6572aec676e9
Vincent Dupain <vdupain@gmail.com>
2024-09-01 19:22:01 +0200

    fix talos image

---
+++ 2-talos/1-talos-vm/talos-image.tf
@@ -0,0 +1,28 @@
+locals {
+  factory_url = "https://factory.talos.dev"
+
+  platform = "nocloud"
+  arch     = "amd64"
+  version  = "v1.7.6"
+  schematic = file("schematic.yaml")
+
+  schematic_id = jsondecode(data.http.schematic_id.response_body)["id"]
+  image_id     = "${local.schematic_id}_${local.version}"
+}
+
+data "http" "schematic_id" {
+  url          = "${local.factory_url}/schematics"
+  method       = "POST"
+  request_body = local.schematic
+}
+
+resource "proxmox_virtual_environment_download_file" "this" {
+  node_name               = var.pve_target_node
+  content_type            = "iso"
+  datastore_id            = "local"
+  decompression_algorithm = "gz"
+  overwrite               = false
+
+  url       = "${local.factory_url}/image/${local.schematic_id}/${local.version}/${local.platform}-${local.arch}.raw.gz"
+  file_name = "talos-${local.schematic_id}-${local.version}-${local.platform}-${local.arch}.img"
+}
