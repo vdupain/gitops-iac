d08f83e754c6461b213acfaa34328d8580ffd204
Vincent Dupain <vincent.dupain@protonmail.com>
2024-09-22 07:20:59 +0000

    fix ipv6

--- 2-talos/2-talos-k8s-cluster/bootstrap.sh
+++ 2-talos/2-talos-k8s-cluster/bootstrap.sh
@@ -3,3 +3,5 @@
 tofu output -raw kubeconfig > kubeconfig-talos-cluster
 tofu output -raw talosconfig > talosconfig-talos-cluster
 mkdir -p $HOME/.kube && cp kubeconfig-talos-cluster $HOME/.kube/config
+
+
