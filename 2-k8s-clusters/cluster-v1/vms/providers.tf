fcdeaadf6d7117d28e439059572cc99b6f7ac104
Vincent Dupain <vincent.dupain@protonmail.com>
2024-10-31 19:09:11 +0000

    fix naming conventions

--- 2-k8s-clusters/cluster-v1/vms/providers.tf
+++ 2-k8s-clusters/cluster-v1/vms/providers.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = ">=0.66.3"
-    }
-  }
-}
-
 provider "proxmox" {
   endpoint  = var.proxmox.endpoint
   api_token = var.proxmox.api_token
